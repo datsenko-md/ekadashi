eee130b1d47650406ed1fed65f470f098b7d3a06
Max Datsenko <datsenko.md@gmail.com>
2024-02-19 17:31:02 +0200

    add getting data from site

--- bin/ekadashi.js
+++ bin/ekadashi.js
@@ -2,15 +2,16 @@
 
 import fs from 'fs';
 import path from 'path';
+import axios from 'axios';
+
 import getEkadashiSchedule from '../src/ekadashi.js';
 
-const calendarsDir = 'calendars';
-const rootDir = path.resolve();
+const city = 'Dnepropetrovsk';
+const country = 'Ukraine';
 const year = new Date().getFullYear();
-const sourcePath = path.resolve(rootDir, calendarsDir, `ekadashi${year}.txt`);
-const destinationPath = path.resolve(rootDir, 'result.txt');
-
-const data = fs.readFileSync(sourcePath, 'utf8');
+const url = `https://www.vaisnavacalendar.info/calendars/${year}/${city}%20[${country}].txt`;
+const destinationPath = path.resolve(path.resolve(), 'result.txt');
+const { data } = await axios.get(url, { responseType: 'text' });
 const content = getEkadashiSchedule(data);
 fs.writeFileSync(destinationPath, content);
 console.log('Result in file >> result.txt');
